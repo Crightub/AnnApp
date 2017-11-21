4efb0e96bdb56f2910abe20f7b3e362b567d358a
AnnApp-GbSA <AnnApp-GbSA@users.noreply.github.com>
2017-11-21 21:08:11 +0100

    Added GUI for adding new grades Next Step: Adding functionallity to GUI

--- app/src/main/java/de/tk/annapp/Fragments/gradesFragment.java
+++ app/src/main/java/de/tk/annapp/Fragments/gradesFragment.java
@@ -1,14 +1,22 @@
 package de.tk.annapp.Fragments;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Debug;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 import java.util.ArrayList;
@@ -31,6 +39,17 @@ public class gradesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         getActivity().setTitle("Noten");
         root = inflater.inflate(R.layout.fragment_grades, container, false);
+
+        FloatingActionButton fabAdd = (FloatingActionButton) root.findViewById(R.id.fabAdd);
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("bla");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    createInputDialog();
+                }
+            }
+        });
 
         recyclerView = root.findViewById(R.id.recyclerViewSubjectsId);
 
@@ -67,6 +86,35 @@ public class gradesFragment extends Fragment {
         System.out.println(mathe.getGradePointAverage());
 
         System.out.println("" + subjectManager.getWholeGradeAverage());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void createInputDialog(){
+
+        AlertDialog.Builder ad = new  AlertDialog.Builder(this.getContext());
+
+
+
+        View mView = getLayoutInflater().inflate(R.layout.fragment_grade_input, null);
+
+        final EditText gradeInput = (EditText) mView.findViewById(R.id.gradeInput);
+        final  EditText ratingInput =(EditText) mView.findViewById(R.id.ratingInput);
+
+        final Spinner subjectSelection = (Spinner) mView.findViewById(R.id.subjectSelection);
+
+        String[] subjectNames;
+
+
+        ad      .setTitle("Note hinzufügen")
+                .setView(mView)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //TODO adding grades to arrayList from Input to correct subject
+
+                    }
+                })
+                .show();
     }
 
 }
