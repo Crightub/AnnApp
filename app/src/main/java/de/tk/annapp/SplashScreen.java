ad8d564dcb6a11f9fc668e67986a30077804e385
AnnApp-GbSA <AnnApp-GbSA@users.noreply.github.com>
2017-12-29 22:48:22 +0100

    Replaced Bootanimation (Better looking but not the final look I like)

--- app/src/main/java/de/tk/annapp/SplashScreen.java
+++ app/src/main/java/de/tk/annapp/SplashScreen.java
@@ -1,22 +1,37 @@
 package de.tk.annapp;
 
+import android.animation.Animator;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.VideoView;
+import android.widget.ViewAnimator;
 
 public class SplashScreen extends Activity {
 
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 800;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
 
+        Button img = (Button) findViewById(R.id.img);
+        Animation in = AnimationUtils.loadAnimation(this, R.anim.in);
+        img.startAnimation(in);
 
         new Handler().postDelayed(new Runnable() {
 
